c08d6cfebffef81cecdd95c64a3129c47f9023f0
taylor Schuchter <taylorschuchter@gmail.com>
2023-03-06 15:09:08 -0500

    Project Complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gameList = new List<string>() { "Skyrim", "Pokemon Red", "Animal Crossing New Horizons", "The Outer Worlds", "Cult of the Lamb", "Stardew Valley", "Death and Taxes" };
+            var gameNameLength = gameList.OrderBy(game => game.Count());
+            foreach (var game in gameNameLength)
+            {
+                Console.WriteLine(game);
+            }            
         }
     }
 }
+//Create a list of video game names...
+//Order the list of games by length of the game name.
+//Use the lambda expression in this exercise as well.
+//use Method Syntax for this exercise
